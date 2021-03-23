3328b9e64e2c3ac21acdf8ec8e13cbab8b80dbb6
Joshua Musiol <joshua.musiol@web.de>
2021-03-23 17:34:14 +0100

    problem solving

--- public/script.js
+++ public/script.js
@@ -1,19 +1,42 @@
-(function () {
+(() => {
     console.log("hola");
 
     new Vue({
         el: "#main",
         data: {
-            title: "imageboard",
             news: "newest additions",
+            title: "",
+            description: "",
+            username: "",
+            file: null,
             images: [],
         },
         mounted: function () {
-            axios.get("/images").then((response) => {
-                console.log(response);
-                console.log("this", this);
-                this.images = response.data;
-            });
+            // keine arrow-function, weil sonst this sich auf window bezieht (Kontext ändert sich)
+            axios
+                .get("/images")
+                .then((response) => (this.images = response.data)); //this bezieht sich hier auf Vue
+        },
+        methods: {
+            onSubmit: function () {
+                console.log("ready for upload");
+                const formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+
+                axios
+                    .post("/images", formData, {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    })
+                    .then((response) => this.images.push(response.detail));
+            },
+            onFileSelect: function () {
+                this.file = this.$refs.image.files[0];
+            },
         },
     });
 })();
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
-
 const { getImages } = require("./db");
+const { uploader } = require("./uploads/uploads");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/images", (request, response) => {
     getImages()
@@ -14,6 +15,31 @@ app.get("/images", (request, response) => {
             console.log("cannot get images:", error);
             response.sendStatus(500);
         });
+});
+
+app.post("/images", uploader.single("file"), (request, response) => {
+    console.log("upload successful", request.file, request.body);
+    response.sendStatus(200).catch((error) => console.log(error));
+});
+/*.then((image) => response.json(image))
+        .catch((error) => {
+            console.log("cannot save image:", error);
+            response.sendStatus(500);
+        });
+});*/
+
+app.get("/upload", (request, response) => {
+    response.send(`
+    <form enctype="multipart/form-data" action="/upload" method="POST">
+        <input type="file" accept="image/*" name="file" required>
+        <button type="submit">Upload</button>
+    </form>
+    `);
+});
+
+app.post("/upload", uploader.single("file"), (request, response) => {
+    console.log("upload successful", request.file, request.body);
+    response.sendStatus(200).catch((error) => console.log(error));
 });
 
 app.listen(8080, () => console.log("server is up and running on port 8080"));
